e3c8e26597234a2a63eb0bc11b81d6f4a764235f
AnantaSrikar <srikarananta01@gmail.com>
2020-02-29 09:56:06 +0530

    Removing db after getting chats

--- backend/retriveChats.py
+++ backend/retriveChats.py
@@ -1,4 +1,5 @@
 import sqlite3
+import os
 connection = sqlite3.connect('msgstore.db')
 cursor = connection.cursor()
 cursor.execute("SELECT data from available_message_view WHERE from_me = 1")
@@ -13,4 +14,5 @@ for r in results:
 fileManager.close()
 cursor.close()
 connection.close()
+os.remove('msgstore.db')
 #from_me = 1, data available_message_view, availabe_messages_view
