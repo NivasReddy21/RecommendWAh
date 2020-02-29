bb5d9cd9c7a82a2b93a6fc522484fe58e6f2cf8d
Nivas <nivas.sarath@gmail.com>
2020-02-29 21:50:28 +0530

    working predict.py

--- backend/predict.py
+++ backend/predict.py
@@ -1,14 +1,12 @@
-import jieba
 import pandas as pd
-import numpy as np
-from surprise import Reader, Dataset, SVD
 from gensim.models import Word2Vec, KeyedVectors
 import nltk
-from gensim import corpora
 from gensim.utils import simple_preprocess
 import ignoreRep
+from nltk.corpus import stopwords
 
-model = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300_1.bin', binary=True, limit=100000)
+model = KeyedVectors.load_word2vec_format(
+    'GoogleNews-vectors-negative300_1.bin', binary=True, limit=100000)
 
 data = pd.read_csv("good_reads_final.csv")
 data = data[["genre_1", "book_title", "book_fullurl"]]
@@ -16,15 +14,34 @@ data = data[["genre_1", "book_title", "book_fullurl"]]
 genres = data.genre_1.unique()
 genre_list = []
 
+
 for genre in genres:
     genre_list.append(genre.lower())
 
-ppg_list = model.wv.most_similar('programming')
+word_list = []
 
-reco_list = []
+for word, count in ignoreRep.returnFreq().most_common(10000):
+    if word not in stopwords.words('english') and word.isalpha() and word not in word_list:
+        word_list.append(word)
 
-for i in ppg_list:
-    reco_list.append(i[0].lower())
+
+recommendation = []
+
+for word1 in word_list:
+    try:
+        ppg_list = model.wv.most_similar(word1)
+        for predict in ppg_list:
+            if predict[0].lower() in genre_list:
+                recommendation.append(predict[0].lower())
+
+    except KeyError:
+        pass
+
+print(stopwords.words('english'))
+
+counter = []
 
 for word, count in ignoreRep.returnFreq().most_common(150):
-        print(word, ": ", count)
+    if word not in stopwords.words('english'):
+        counter.append(count)
+        print(word + ':' + str(count))
