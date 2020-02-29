5f748d62073c7d4822e033740e3a601b9eedad67
AnantaSrikar <srikarananta01@gmail.com>
2020-03-01 02:04:11 +0530

    Made it slightly better

--- recommendWAh/fileUpload/urls.py
+++ recommendWAh/fileUpload/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls import url
 from .views import model_form_upload
 
 urlpatterns = [
-    url('', model_form_upload, name='upload')
+    url('', model_form_upload, name='upload'),
 ]
