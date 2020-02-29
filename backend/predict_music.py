fe3bf7f2ac748c69d653e1c1c0cf26798bdca246
AnantaSrikar <srikarananta01@gmail.com>
2020-03-01 01:25:01 +0530

    Getting ready to add backend

--- backend/predict_music.py
+++ backend/predict_music.py
@@ -6,43 +6,45 @@ from gensim.utils import simple_preprocess
 import ignoreRep
 from nltk.corpus import stopwords
 
-model = KeyedVectors.load_word2vec_format('trained_data.bin', binary=True, limit=100000)
+def get_music_reccom():
 
-data = pd.read_csv("Databases/music.csv")
-data = data[["terms", "title", "artist.name"]]
+    model = KeyedVectors.load_word2vec_format('trained_data.bin', binary=True, limit=100000)
 
-genres = data.terms.unique()
-genre_list = []
+    data = pd.read_csv("Databases/music.csv")
+    data = data[["terms", "title", "artist.name"]]
 
-
-for genre in genres:
-    genre_list.append(genre)
-
-word_list = []
-
-for word, count in ignoreRep.returnFreq().most_common(10000):
-    if word not in stopwords.words('english') and word.isalpha() and word not in word_list:
-        word_list.append(word)
+    genres = data.terms.unique()
+    genre_list = []
 
 
-recommendation = []
+    for genre in genres:
+        genre_list.append(genre)
 
-for word1 in word_list:
-    try:        
-        ppg_list = model.wv.most_similar(word1)
-        for predict in ppg_list:
-            if predict[0].lower() in genre_list:
-                recommendation.append(predict[0].lower())
-    except KeyError:
-        pass
+    word_list = []
 
-counter = []
-completedReco = []
+    for word, count in ignoreRep.returnFreq().most_common(10000):
+        if word not in stopwords.words('english') and word.isalpha() and word not in word_list:
+            word_list.append(word)
 
-for reco in recommendation:
-    word_counter = collections.Counter(recommendation)
 
-for word, count in word_counter.most_common(100):
-        completedReco.append(word)
+    recommendation = []
 
-print(completedReco)
+    for word1 in word_list:
+        try:        
+            ppg_list = model.wv.most_similar(word1)
+            for predict in ppg_list:
+                if predict[0].lower() in genre_list:
+                    recommendation.append(predict[0].lower())
+        except KeyError:
+            pass
+
+    counter = []
+    completedReco = []
+
+    for reco in recommendation:
+        word_counter = collections.Counter(recommendation)
+
+    for word, count in word_counter.most_common(100):
+            completedReco.append(word)
+
+    return(completedReco[0])
